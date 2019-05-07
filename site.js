0d44a092ca7978eecd4c40dceb253168b8e29627
Mr. BS Mthethwa <u18098607@up.ac.za>
2019-05-07 14:50:20 +0200

    'hklhl'

---
+++ site.js
@@ -0,0 +1,5 @@
+
+function BaMessage() {
+    document.getElementById("BM").innerHTML = "I am excited about INF354 because I want to extend my conding knowledge so i can become an expert in my field";
+}
+
