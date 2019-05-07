11777707f7ce6b79ffb2f22a7fab0eb3ab1697dc
unknown <User@INFST-44.up.ac.za>
2019-05-07 14:55:25 +0200

    tyjti

--- site.js
+++ site.js
@@ -1,4 +1,4 @@
-
+wertgrkiuyiky
 function BaMessage() {
     document.getElementById("BM").innerHTML = "I am excited about INF354 because I want to extend my conding knowledge so i can become an expert in my field";
 }
